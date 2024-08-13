446b99471bbbaf5b37511bb58b3586d73d214e0e
tx-nozaki <51816794+tx-nozaki@users.noreply.github.com>
2024-08-13 07:33:12 +0000

    Update Apps

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,20 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data, MODEL_ID, generateHaikuFunction } from "./data/resource";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
-defineBackend({
+export const backend = defineBackend({
   auth,
   data,
+  generateHaikuFunction,
 });
+
+backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ["bedrock:InvokeModel"],
+    resources: [
+      `arn:aws:bedrock:*::foundation-model/${MODEL_ID}`,
+    ],
+  })
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,53 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { FormEvent, useState } from "react";
+
+import { generateClient } from "aws-amplify/api";
+import { Schema } from "@/amplify/data/resource";
+
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+
+Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+export default function App() {
+  const [prompt, setPrompt] = useState<string>("");
+  const [answer, setAnswer] = useState<string | null>(null);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+  const sendPrompt = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { data, errors } = await client.queries.generateHaiku({
+      prompt: "Frank Herbert's Dune",
     });
-  }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+    if (!errors) {
+      setAnswer(data);
+      setPrompt("");
+    } else {
+      console.log(errors);
+    }
+  };
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 dark:text-white">
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+        <h1 className="text-3xl font-bold text-center mb-4">Haiku Generator</h1>
+
+        <form className="mb-4 self-center max-w-[500px]" onSubmit={sendPrompt}>
+          <input
+            className="text-black p-2 w-full"
+            placeholder="Enter a prompt..."
+            name="prompt"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+        </form>
+
+        <div className="text-center">
+          <pre>{answer}</pre>
+        </div>
       </div>
     </main>
   );
 }
-
-export default App;
